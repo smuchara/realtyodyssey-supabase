00fe021d8f602b865451d3b408531565a6bf1012
stephenmuchara <stephen.muchara@elaralabs.ai>
2026-04-11 11:41:16 +0300

    CI/CD Production Fix

--- functions/_shared/supabase.ts
+++ functions/_shared/supabase.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "npm:@supabase/supabase-js@2";
 
 function getEnv(name: string) {
   const value = Deno.env.get(name);
